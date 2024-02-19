408a9fc25dbe04c1c39c6c72b07ae5d4e476dbef
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-19 10:42:44 +0600

    route restricted

--- src/App.js
+++ src/App.js
@@ -29,6 +29,8 @@ import AddProjects from "./Components/Teacher/TeacherProject/AddProjects";
 import TeacherViewProjects from "./Components/Teacher/TeacherViewProjects/TeacherViewProjects";
 
 function App() {
+  const userRole = localStorage.getItem("userRole");
+
   return (
     <div>
       <Navbar />
@@ -36,48 +38,62 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/board" element={<Container />} />
+        {userRole === "teacher" && (
+          // teacher routes
+          <>
+            <Route
+              path="/teacherRegistrationForm"
+              element={<TeacherRegistrationForm />}
+            />
+            <Route path="/teacherProfile" element={<TeacherProfile />} />
+            <Route path="/teacherCourse" element={<TeacherCourse />} />
+            <Route
+              path="/teacherCourseViewStudent"
+              element={<CourseViewStudent />}
+            />
+            <Route
+              path="/teacherViewProject"
+              element={<TeacherViewProjects />}
+            />
+            <Route path="/teacherResource" element={<TeacherResource />} />
+            <Route path="/teacherProject" element={<TeacherProject />} />
+            <Route path="/teacherAddProject" element={<AddProjects />} />
+            <Route
+              path="/teacherProjectViewStudent"
+              element={<ViewStudents />}
+            />
+            <Route
+              path="/teacherProjectViewStudent/:id"
+              element={<ViewStudents />}
+            />
+          </>
+        )}
 
-        {/* teacher  */}
-        <Route
-          path="/teacherRegistrationForm"
-          element={<TeacherRegistrationForm />}
-        />
-        <Route path="/teacherProfile" element={<TeacherProfile />} />
-        <Route path="/teacherCourse" element={<TeacherCourse />} />
-        <Route
-          path="/teacherCourseViewStudent"
-          element={<CourseViewStudent />}
-        />
-        <Route path="/teacherViewProject" element={<TeacherViewProjects />} />
-        <Route path="/teacherResource" element={<TeacherResource />} />
-        <Route path="/teacherProject" element={<TeacherProject />} />
-        <Route path="/teacherAddProject" element={<AddProjects />} />
-        <Route path="/teacherProjectViewStudent" element={<ViewStudents />} />
-        <Route
-          path="/teacherProjectViewStudent/:id"
-          element={<ViewStudents />}
-        />
-
-        {/* student  */}
-        <Route
-          path="/studentRegistrationForm"
-          element={<StudentRegistrationForm />}
-        />
-        <Route path="/studentProfile" element={<StudentProfile />} />
-        <Route path="/studentCourse" element={<StudentCourse />} />
-        <Route path="/myCourse" element={<MyCourses />} />
-        <Route path="/studentAssignment" element={<StudentAssignment />} />
-        <Route
-          path="/studentCollaboration"
-          element={<StudentCollaboration />}
-        />
-        <Route path="/uploadFile" element={<UploadFile />} />
-        <Route path="/studentResource" element={<StudentResource />} />
-        <Route path="/studentProject" element={<StudentProject />} />
-        <Route path="/studentAnalytics" element={<StudentAnalytics />}>
-          <Route index element={<StudentFeedback />}></Route>
-          <Route path="studentReward" element={<StudentReward />}></Route>
-        </Route>
+        {userRole === "student" && (
+          // student routes
+          <>
+            {" "}
+            <Route
+              path="/studentRegistrationForm"
+              element={<StudentRegistrationForm />}
+            />
+            <Route path="/studentProfile" element={<StudentProfile />} />
+            <Route path="/studentCourse" element={<StudentCourse />} />
+            <Route path="/myCourse" element={<MyCourses />} />
+            <Route path="/studentAssignment" element={<StudentAssignment />} />
+            <Route
+              path="/studentCollaboration"
+              element={<StudentCollaboration />}
+            />
+            <Route path="/uploadFile" element={<UploadFile />} />
+            <Route path="/studentResource" element={<StudentResource />} />
+            <Route path="/studentProject" element={<StudentProject />} />
+            <Route path="/studentAnalytics" element={<StudentAnalytics />}>
+              <Route index element={<StudentFeedback />} />
+              <Route path="studentReward" element={<StudentReward />} />
+            </Route>
+          </>
+        )}
 
         <Route path="*" element={<Error />} />
       </Routes>
