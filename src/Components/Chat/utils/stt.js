6a0ed3ff5b6a34e01a02486bd432131c01095d3c
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-03-09 11:23:56 +0600

    chatbot

--- src/Components/Chat/utils/stt.js
+++ src/Components/Chat/utils/stt.js
@@ -3,31 +3,34 @@ let serviceRegion = "southcentralus"; // e.g., "westus"
 
 const sdk = require("microsoft-cognitiveservices-speech-sdk");
 
-async function textToSpeech(text,speaking,setSpeaking) {
-    console.log(text)
-    let player = new sdk.SpeakerAudioDestination();
-    let speechConfig = sdk.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
-    speechConfig.speechSynthesisVoiceName = "bn-BD-NabanitaNeural";
-    let audioConfig = sdk.AudioConfig.fromSpeakerOutput(player);
-    let synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
-    setSpeaking(true);
-    player.onAudioEnd = () => {
-        setSpeaking(false);
+async function textToSpeech(text, speaking, setSpeaking) {
+  console.log(text);
+  let player = new sdk.SpeakerAudioDestination();
+  let speechConfig = sdk.SpeechConfig.fromSubscription(
+    subscriptionKey,
+    serviceRegion
+  );
+  speechConfig.speechSynthesisVoiceName = "bn-BD-NabanitaNeural";
+  let audioConfig = sdk.AudioConfig.fromSpeakerOutput(player);
+  let synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+  setSpeaking(true);
+  player.onAudioEnd = () => {
+    setSpeaking(false);
+  };
+
+  synthesizer.speakTextAsync(
+    text,
+    (result) => {
+      if (result) {
+        console.log(`STATUS: ${result.reason}`);
+      }
+      synthesizer.close();
+    },
+    (error) => {
+      console.log(`ERROR: ${error}`);
+      synthesizer.close();
     }
-    
-    synthesizer.speakTextAsync(
-        text,
-        result => {
-            if (result) {
-                console.log(`STATUS: ${result.reason}`);
-            }
-            synthesizer.close();
-        },
-        error => {
-            console.log(`ERROR: ${error}`);
-            synthesizer.close();
-        }
-    )
+  );
 }
 
 export default textToSpeech;
